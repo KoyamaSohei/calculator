daaf939e2d4c0914cdcb33089b04a6d1472ae85f
KoyamaSohei <koyamaso0309@gmail.com>
2020-03-11 17:59:12 +0900

    impliment: eval()

--- calculator.go
+++ calculator.go
@@ -1,5 +1,13 @@
 package calculator
 
-func eval(s string) int {
-	return 0
+func eval(s string) (int, error) {
+	e, err := parse(s)
+	if err != nil {
+		return -1, err
+	}
+	e, err = shuntingyard(e)
+	if err != nil {
+		return -1, err
+	}
+	return evalPostfix(e)
 }
